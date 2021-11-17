81f30010a73f85d22251dfcb42ab980c57f247cd
denisesenguel <denisesenguel@gmail.com>
2021-11-17 18:25:48 +0100

    call main fcn correctly and remove some auxiliary print statements

--- src/data/stack_bands.py
+++ src/data/stack_bands.py
@@ -12,10 +12,8 @@ def main(file_list, path_out):
     with rasterio.open(path_out, "w", **meta) as dst:
     
         for id, layer in enumerate(file_list, start=1):
-            print(id, layer)
             with rasterio.open(layer) as src1:
                 band = src1.read(1)
-                print(band[~np.isnan(band)].max())
                 dst.write_band(id, src1.read(1))
 
 if __name__ == "__main__":
@@ -24,4 +22,8 @@ if __name__ == "__main__":
 
     parser.add_argument('--input', nargs='+', help='input image file names (required)', required=True)
     parser.add_argument('--output', help='output image file name (required)', required=True)
+
+    args = parser.parse_args()
+
+    main(file_list=args.input, path_out=args.output)
 
